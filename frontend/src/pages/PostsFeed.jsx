7e8ab95ebc29ca925cf255c34c059e9d6636aa46
Satvik <satvik2006sharma@gmail.com>
2026-01-31 10:44:01 +0530

    feat: made some changes in Posts feed

--- frontend/src/pages/PostsFeed.jsx
+++ frontend/src/pages/PostsFeed.jsx
@@ -4,12 +4,16 @@ import api from "../api/axios"
 const PostsFeed = () => {
   const [posts, setPosts] = React.useState([])
   const [loading, setLoading] = React.useState(true)
+  const [page, setPage] = React.useState(1)
+  const [search, setSearch] = React.useState("")
+  const [totalPages, setTotalPages] = React.useState(1)
 
   React.useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await api.get("/post")
-        setPosts(Array.isArray(res.data.posts) ? res.data.posts : [])
+        const res = await api.get(`/post?search=${search}&page=${page}&limit=6`)
+        setPosts(prev => page === 1 ? res.data.posts : [...prev, ...res.data.posts])
+        setTotalPages(res.data.totalPosts)
       } catch (err) {
         console.error(err)
         setPosts([])
@@ -19,13 +23,23 @@ const PostsFeed = () => {
     }
 
     fetchPosts()
-  }, [])
+  }, [page, search])
 
   if (loading) return <p>Loading posts...</p>
 
   return (
     <div className="container">
       <h1>Posts</h1>
+
+      <input
+        type="text"
+        placeholder="Search any blog"
+        value = {search}
+        onChange={(e)=> {
+          setSearch(e.target.value)
+          setPage(1)}
+        }
+      />
 
       {posts.length === 0 && <p>No posts yet</p>}
 
@@ -35,6 +49,12 @@ const PostsFeed = () => {
           <p>{post.content}</p>
         </div>
       ))}
+
+      {page < totalPages && (
+        <button onClick={() => setPage(p => p + 1)}>
+          Load more
+        </button>
+      )}
     </div>
   )
 }
